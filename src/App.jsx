b67fb4926c53ae1bc4b1512dcdca3e346cda76f2
Wojcik Michal <m.mwojcik@outlook.com>
2021-02-10 23:07:52 +0100

    add base background and fix issue with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-// import logo from './logo.svg';
-// import './App.css';
-import { Container, Component } from 'react';
+import './App.css';
+import { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
-// import VotingContract from './contracts/Voting.json';
 
 import HomePage from './pages/HomePage';
 import CampaignPage from './pages/CampaignPage';
@@ -13,34 +11,40 @@ import NotFoundPage from './pages/NotFoundPage';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-    console.log(HomePage);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-  render() {
-    return (
-      <Router>
-        <Container>
-          <Menu secondary>
-            <Menu.Item name="home">
-              <Link to="/" />
-            </Menu.Item>
-          </Menu>
+    render() {
+        return (
+            <Router>
+                <Menu inverted>
+                    <Menu.Item as={Link} to='/'>Home</Menu.Item>
+                    <Menu.Item as={Link} to='/campaign/new'>Create Campaign</Menu.Item>
+                </Menu>
 
-          <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route path='campaign/:campaignId' component={CampaignPage} />
-            <Route path='campaign/new' component={CreateCampaignPage} />
-            <Route path='vote/:campaignId' component={VotePage} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </Container>
-      </Router>
-    )
-  }
+                <Switch>
+                    <Route exact path='/'>
+                        <HomePage/>
+                    </Route>
+                    <Route path='/campaign/new'>
+                        <CreateCampaignPage/>
+                    </Route>
+                    <Route path='campaign/:campaignId'>
+                        <CampaignPage/>
+                    </Route>
+                    <Route path='/vote/:campaignId'>
+                        <VotePage/>
+                    </Route>
+                    <Route>
+                        <NotFoundPage/>
+                    </Route>
+                </Switch>
+
+            </Router>
+        )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
