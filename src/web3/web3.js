d5c7374ad058054a25c59ff2ca5a8f4a898cba28
Wojcik Michal <m.mwojcik@outlook.com>
2021-03-05 23:37:34 +0100

    fix deprecated web3 api call

--- src/web3/web3.js
+++ src/web3/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 
-export const web3 = new Web3(window.web3.currentProvider);
+export const web3 = new Web3(window.ethereum);
 
 export const tryLogin = async () => await window.ethereum.request({method: 'eth_requestAccounts'});
 
